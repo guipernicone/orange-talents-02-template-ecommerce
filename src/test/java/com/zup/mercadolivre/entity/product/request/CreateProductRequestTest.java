c12fe2529df8ab9337aafc85f4d6572fdfa54c82
guipernicone <guipernicone@hotmail.com>
2021-03-01 10:33:52 -0300

    Atualiza teste unitario para CreateProductRequest

--- src/test/java/com/zup/mercadolivre/entity/product/request/CreateProductRequestTest.java
+++ src/test/java/com/zup/mercadolivre/entity/product/request/CreateProductRequestTest.java
@@ -1,78 +1,110 @@
-//package com.zup.mercadolivre.entity.product.request;
-//
-//import com.zup.mercadolivre.entity.category.Category;
-//import com.zup.mercadolivre.entity.product.Product;
-//import com.zup.mercadolivre.entity.product.response.ProductResponse;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mock;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//import javax.persistence.EntityManager;
-//import java.math.BigDecimal;
-//import java.util.Arrays;
-//
-//import static org.mockito.Mockito.when;
-//
-//@SpringBootTest
-//@ActiveProfiles("test")
-//public class CreateProductRequestTest {
-//
-//    @Mock
-//    private EntityManager mockEntityManager;
-//
-//    @Test
-//    public void testToModelCategoryValidId(){
-//        String productName = "productTest";
-//        String categoryId = "1";
-//        CreateProductRequest productRequest = new CreateProductRequest(
-//                productName,
-//                new BigDecimal(1),
-//                1,
-//                Arrays.asList(
-//                        new CreateProductCharacteristicsRequest("detailName", "value"),
-//                        new CreateProductCharacteristicsRequest("detailName", "value"),
-//                        new CreateProductCharacteristicsRequest("detailName", "value")
-//                ),
-//                "description",
-//                categoryId
-//        );
-//
-//        Category category = new Category("test", null);
-//        when(mockEntityManager.find(Category.class, Long.parseLong(categoryId))).thenReturn(category);
-//
-//        Product product = productRequest.toModel(mockEntityManager);
-//        Assertions.assertNotNull(product.getCategory());
-//        Assertions.assertNotNull(product.getProductCharacteristics());
-//        Assertions.assertEquals(productName, product.getName());
-//
-//    }
-//
-//    @Test
-//    public void testToModelCategoryNotValidId(){
-//        String productName = "productTest";
-//        String categoryId = "1";
-//        CreateProductRequest productRequest = new CreateProductRequest(
-//                productName,
-//                new BigDecimal(1),
-//                1,
-//                Arrays.asList(
-//                        new CreateProductCharacteristicsRequest("detailName", "value"),
-//                        new CreateProductCharacteristicsRequest("detailName", "value"),
-//                        new CreateProductCharacteristicsRequest("detailName", "value")
-//                ),
-//                "description",
-//                categoryId
-//        );
-//
-//        Category category = new Category("test", null);
-//        when(mockEntityManager.find(Category.class, Long.parseLong(categoryId))).thenReturn(null);
-//
-//        IllegalArgumentException exception =
-//                Assertions.assertThrows(IllegalArgumentException.class, () -> {
-//                    productRequest.toModel(mockEntityManager);
-//                });
-//        Assertions.assertEquals("The category with id (" +categoryId+") was not found", exception.getMessage());
-//    }
-//}
+package com.zup.mercadolivre.entity.product.request;
+
+import com.zup.mercadolivre.entity.category.Category;
+import com.zup.mercadolivre.entity.product.Product;
+import com.zup.mercadolivre.entity.user.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class CreateProductRequestTest {
+
+    @Mock
+    private EntityManager mockEntityManager;
+
+    @Mock
+    private Query query;
+
+    private User userTest;
+    private Category categoryTest;
+    private CreateProductRequest productRequestTest;
+
+    @BeforeEach
+    public void Setup(){
+        this.productRequestTest = new CreateProductRequest(
+                "productTest",
+                new BigDecimal(1),
+                1,
+                Arrays.asList(
+                        new CreateProductCharacteristicsRequest("detailName", "value"),
+                        new CreateProductCharacteristicsRequest("detailName", "value"),
+                        new CreateProductCharacteristicsRequest("detailName", "value")
+                ),
+                "description",
+                "1"
+        );
+       userTest = new User("test@zup.com.br", "123456");
+       categoryTest = new Category("test", null);
+    }
+
+    @Test
+    public void testToModelCategoryValidId(){
+        Mockito.when(
+                mockEntityManager.find(Category.class,
+                Long.parseLong(productRequestTest.getCategoryId()))
+        ).thenReturn(categoryTest);
+        Mockito.when(mockEntityManager.createQuery(ArgumentMatchers.anyString())).thenReturn(query);
+        Mockito.when(query.setParameter(ArgumentMatchers.anyString(), ArgumentMatchers.anyString())).thenReturn(query);
+        Mockito.when(query.getSingleResult()).thenReturn(userTest);
+
+        Product product = productRequestTest.toModel(mockEntityManager, userTest.getLogin());
+
+        Assertions.assertNotNull(product.getCategory());
+        Assertions.assertNotNull(product.getProductCharacteristics());
+        Assertions.assertEquals(productRequestTest.getName(), product.getName());
+        Assertions.assertNotNull(product.getOwner());
+        Assertions.assertEquals(userTest.getLogin(),product.getOwner().getLogin());
+
+    }
+
+    @Test
+    public void testToModelCategoryNotValidId(){
+        Mockito.when(mockEntityManager.find(
+                Category.class,
+                Long.parseLong(productRequestTest.getCategoryId()))
+        ).thenReturn(null);
+        Mockito.when(mockEntityManager.createQuery(ArgumentMatchers.anyString())).thenReturn(query);
+        Mockito.when(query.setParameter(ArgumentMatchers.anyString(), ArgumentMatchers.anyString())).thenReturn(query);
+        Mockito.when(query.getSingleResult()).thenReturn(userTest);
+
+        IllegalArgumentException exception =
+                Assertions.assertThrows(IllegalArgumentException.class, () -> {
+                    productRequestTest.toModel(mockEntityManager, userTest.getLogin());
+                });
+        Assertions.assertEquals(
+                "The category with id (" +productRequestTest.getCategoryId() +") was not found",
+                exception.getMessage()
+        );
+
+    }
+
+    @Test
+    public void testToModelUserNotValidId(){ ;
+        Mockito.when(mockEntityManager.find(
+                Category.class,
+                Long.parseLong(productRequestTest.getCategoryId()))
+        ).thenReturn(categoryTest);
+        Mockito.when(mockEntityManager.createQuery(ArgumentMatchers.anyString())).thenReturn(query);
+        Mockito.when(query.setParameter(ArgumentMatchers.anyString(), ArgumentMatchers.anyString())).thenReturn(query);
+        Mockito.when(query.getSingleResult()).thenReturn(null);
+
+        IllegalArgumentException exception =
+                Assertions.assertThrows(IllegalArgumentException.class, () -> {
+                    productRequestTest.toModel(mockEntityManager, userTest.getLogin());
+                });
+        Assertions.assertEquals("Invalid user id", exception.getMessage());
+    }
+}
